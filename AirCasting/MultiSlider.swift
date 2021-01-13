10b0245f4709d6bc6d804d5c59d5b98b02bc27f4
Monika <monika.smialko@lunarlogic.io>
2021-01-13 14:43:30 +0100

    Add colors

--- AirCasting/MultiSlider.swift
+++ AirCasting/MultiSlider.swift
@@ -11,10 +11,18 @@ struct MultiSlider: View {
     
     var maxValue: Float = 100
     var values: [Float] = [43, 56, 78]
+    var colors: [Color] = [Color.chartGreen, Color.chartYellow, Color.chartOrange, Color.chartRed]
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
+                ForEach(values.indices.reversed(), id: \.self) { index in
+                    colors[index]
+                        .frame(width: CGFloat(values[index]) * geometry.frame(in: .local).size.width / CGFloat(maxValue))
+                        .position(x: CGFloat(values[index]) * geometry.frame(in: .local).size.width / CGFloat(maxValue) / 2,
+                                  y:  geometry.frame(in: .local).size.height / 2)
+                }
+                
                 ForEach(values, id: \.self) { value in
                     button
                         .position(x: CGFloat(value) * geometry.frame(in: .local).size.width / CGFloat(maxValue),
@@ -22,6 +30,8 @@ struct MultiSlider: View {
                 }
             }
         }
+        .frame(height: 10)
+        .padding()
     }
     
     var button: some View {
