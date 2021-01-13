5073475173a550626465fb5963b6a7ce60b9d57f
Monika <monika.smialko@lunarlogic.io>
2021-01-13 11:29:42 +0100

    Graph button navigate to graph view

---
+++ AirCasting/GraphView.swift
@@ -0,0 +1,24 @@
+//
+//  GraphView.swift
+//  AirCasting
+//
+//  Created by Lunar on 13/01/2021.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        VStack{
+            SessionHeader {}
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
--- AirCasting/SessionCell.swift
+++ AirCasting/SessionCell.swift
@@ -12,36 +12,41 @@ struct SessionCell: View {
     @State private var isCollapsed = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 13) {
-            SessionHeader {
-                withAnimation {
-                    isCollapsed = !isCollapsed
+        
+        NavigationView {
+            VStack(alignment: .leading, spacing: 13) {
+                SessionHeader {
+                    withAnimation {
+                        isCollapsed = !isCollapsed
+                    }
+                }
+                
+                if !isCollapsed {
+                    VStack(alignment: .trailing, spacing: 40) {
+                        pollutionChart
+                        buttons
+                    }
                 }
             }
-            
-            if !isCollapsed {
-                VStack(alignment: .trailing, spacing: 40) {
-                    pollutionChart
-                    buttons
-                }
-            }
+            .font(Font.moderate(size: 13, weight: .regular))
+            .foregroundColor(.aircastingGray)
+            .padding()
+            .background(
+                Color.white
+                    .shadow(color: Color(red: 205/255, green: 209/255, blue: 214/255, opacity: 0.36), radius: 9, x: 0, y: 1)
+            )
         }
-        .font(Font.moderate(size: 13, weight: .regular))
-        .foregroundColor(.aircastingGray)
-        .padding()
-        .background(
-            Color.white
-                .shadow(color: Color(red: 205/255, green: 209/255, blue: 214/255, opacity: 0.36), radius: 9, x: 0, y: 1)
-        )
     }
-    
     var pollutionChart: some View {
         PollutionChart()
             .frame(height: 200)
     }
     var graphButton: some View {
-        Button("graph") {}
+            NavigationLink(destination: GraphView()) {
+                Text("graph")
+            }
     }
+    
     var mapButton: some View {
         Button("map") {}
     }
