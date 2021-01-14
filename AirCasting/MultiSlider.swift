3272c9792f8f08a46b496d967a7c8e16c4d5319e
Monika <monika.smialko@lunarlogic.io>
2021-01-14 09:56:02 +0100

    Add value labels

--- AirCasting/MultiSlider.swift
+++ AirCasting/MultiSlider.swift
@@ -15,30 +15,54 @@ struct MultiSlider: View {
     
     var body: some View {
         GeometryReader { geometry in
+            let frameWidth = geometry.frame(in: .local).size.width
+            
             ZStack {
+                colors.last
+                
                 ForEach(values.indices.reversed(), id: \.self) { index in
                     colors[index]
-                        .frame(width: CGFloat(values[index]) * geometry.frame(in: .local).size.width / CGFloat(maxValue))
-                        .position(x: CGFloat(values[index]) * geometry.frame(in: .local).size.width / CGFloat(maxValue) / 2,
+                        .frame(width: CGFloat(values[index]) * frameWidth / CGFloat(maxValue))
+                        .position(x: CGFloat(values[index]) * frameWidth / CGFloat(maxValue) / 2,
                                   y:  geometry.frame(in: .local).size.height / 2)
                 }
                 
                 ForEach(values, id: \.self) { value in
-                    button
-                        .position(x: CGFloat(value) * geometry.frame(in: .local).size.width / CGFloat(maxValue),
+                    sliderButton
+                        .position(x: CGFloat(value) * frameWidth / CGFloat(maxValue),
                                   y: geometry.frame(in: .local).size.height / 2)
                 }
+                labels(geometry: geometry)
             }
+
         }
-        .frame(height: 10)
+        .frame(height: 5)
         .padding()
     }
     
-    var button: some View {
-        Color.red
-            .frame(width: 30, height: 30)
-            .clipShape(Circle())
+    var sliderButton: some View {
+            Color.white
+                .frame(width: 15, height: 15)
+                .clipShape(Circle())
+                .shadow(color: Color(red: 156/255, green: 155/255, blue: 155/255, opacity: 0.5), radius: 9)
     }
+    
+    func labels(geometry: GeometryProxy) -> some View {
+        let frameWidth = geometry.frame(in: .local).size.width
+        let y = geometry.frame(in: .local).size.height / 2
+        
+        return ForEach(values.indices, id: \.self) { index in
+            let ints = Int(values[index])
+            Text("\(ints)")
+                .position(x: CGFloat(values[index]) * frameWidth / CGFloat(maxValue),
+                          y: y)
+                .foregroundColor(.aircastingGray)
+                .font(Font.muli(size: 12))
+                .offset(x: 0, y: 20)
+        }
+    }
+    
+    
 }
 
 struct MultiSlider_Previews: PreviewProvider {
