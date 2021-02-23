e4e3bacb66fa8d847a67e103f3713b099277a6b9
Monika Śmiałko <monika.smialko@gmail.com>
2021-02-23 10:36:55 +0100

    Create account view wip

---
+++ AirCasting/Auth/CreateAccountView.swift
@@ -0,0 +1,44 @@
+//
+//  CreateAccountView.swift
+//  AirCasting
+//
+//  Created by Lunar on 23/02/2021.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+
+    
+    var body: some View {
+        VStack {
+            titleLabel
+            
+        }
+    }
+    
+    var titleLabel: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text("Create account")
+                .font(Font.moderate(size: 32,
+                                    weight: .bold))
+                .foregroundColor(.accentColor)
+            Text("to record and map your envitonment")
+                .font(Font.muli(size: 16))
+                .foregroundColor(.aircastingGray)
+        }
+    }
+    
+//    var username: some View {
+//    }
+//    
+}
+
+struct CreateAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountView()
+    }
+}
