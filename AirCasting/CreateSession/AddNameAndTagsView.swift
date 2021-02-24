42991c4329db3ed5e9e55789d2e5c641910a42d0
Anna Olak <anna.olak1901@gmail.com>
2021-02-24 16:03:48 +0100

    Create view for adding name and tags to new session and fix styling in CreateSessionView

---
+++ AirCasting/CreateSession/AddNameAndTagsView.swift
@@ -0,0 +1,54 @@
+//
+//  AddNameAndTagsView.swift
+//  AirCasting
+//
+//  Created by Anna Olak on 24/02/2021.
+//
+
+import SwiftUI
+
+struct AddNameAndTagsView: View {
+    @State var sessionName = ""
+    @State var sessionTags = ""
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 50) {
+            ProgressView(value: 0.75)
+            titleLabel()
+            createTextfield(placeholder: "Session name", binding: $sessionName)
+            createTextfield(placeholder: "Tags", binding: $sessionTags)
+            Spacer()
+            Button(action: {}, label: {
+                Text("Continue")
+                    .bold()
+            })
+            .buttonStyle(BlueButtonStyle())
+        }
+        .padding()
+    }
+}
+
+struct AddNameAndTagsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNameAndTagsView()
+    }
+}
+
+func createTextfield(placeholder: String, binding: Binding<String> ) -> some View {
+  TextField(placeholder,
+       text: binding)
+    .padding()
+    .frame(height: 50)
+    .background(Color.aircastingGray.opacity(0.05))
+    .border(Color.aircastingGray.opacity(0.1))
+}
+
+
+
+struct titleLabel: View {
+    var body: some View {
+        Text("New session details")
+            .font(Font.moderate(size: 24, weight: .bold))
+            .foregroundColor(.darkBlue)
+    }
+}
--- AirCasting/CreateSession/ConfirmCreatingSession.swift
+++ AirCasting/CreateSession/ConfirmCreatingSession.swift
@@ -13,31 +13,26 @@ struct ConfirmCreatingSession: View {
     var sessionName = "Ania's microphone session"
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 50) {
             Text("Are you ready?")
                 .font(Font.moderate(size: 24, weight: .bold))
                 .foregroundColor(.darkBlue)
-                .multilineTextAlignment(.leading)
-                .padding(.bottom, 15)
-            Text("Your \(sessionType) session \(sessionName) is ready to start gathering data.")
-                .font(Font.muli(size: 16))
-                .foregroundColor(Color.aircastingGray)
-                .multilineTextAlignment(.leading)
-                .lineSpacing(9.0)
-                .padding(.bottom, 15)
-            Text("Move to your starting location, confirm your location is accurate on the map, then press the start recording button below.")
-                .font(Font.muli(size: 16))
-                .foregroundColor(Color.aircastingGray)
-                .multilineTextAlignment(.leading)
-                .lineSpacing(9.0)
+                
+            VStack(spacing: 15) {
+                Text("Your \(sessionType) session \(sessionName) is ready to start gathering data.")
+                Text("Move to your starting location, confirm your location is accurate on the map, then press the start recording button below.")
+            }
+            .font(Font.muli(size: 16))
+            .foregroundColor(Color.aircastingGray)
+            .multilineTextAlignment(.leading)
+            .lineSpacing(9.0)
+            
             GoogleMapView(pathPoints: [], values: [])
                         
-            Spacer()
             Button(action: {}, label: {
                 Text("Start recording")
                     .bold()
             })
-//            .frame(maxWidth: 250)
             .buttonStyle(BlueButtonStyle())
         }
         .padding()
