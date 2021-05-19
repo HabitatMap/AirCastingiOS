fe5fbfc05dbd2344a7aec76c298c3b92f93bd3cd
Monika Śmiałko <monika.smialko@gmail.com>
2021-05-19 14:43:51 +0200

    Display real data on graph

--- AirCasting/Models/Session+Mock.swift
+++ AirCasting/Models/Session+Mock.swift
@@ -15,8 +15,25 @@ extension SessionEntity {
         let session: SessionEntity = try! context.newOrExisting(uuid: SessionUUID(rawValue: "mock")!)
         session.type = .mobile
         // ...
+                
         return session
     }
 }
 
+extension MeasurementStreamEntity {
+    
+    static var mock: MeasurementStreamEntity {
+        let context = PersistenceController.shared.viewContext
+        let stream: MeasurementStreamEntity = try! context.newOrExisting(id: 1_001)
+        
+        for i in 0...10 {
+            let measuremennt: MeasurementEntity = try! context.newOrExisting(id: Int64(i))
+            measuremennt.value = Double(arc4random() % 150)
+            measuremennt.time = Date().addingTimeInterval(-3600).addingTimeInterval(10 * Double(i))
+        }
+        
+        return stream
+    }
+    
+}
 #endif
