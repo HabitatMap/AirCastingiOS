de39c3b4c4136d58d38d3861b7813ca295523a7f
Jan Krzempek <jankrzempek@Jans-MacBook-Pro.local>
2021-06-23 14:15:51 +0200

    fix after CR

---
+++ AirCasting/Utils/Strings.swift
@@ -0,0 +1,36 @@
+// Created by Lunar on 23/06/2021.
+//
+
+import Foundation
+
+struct Strings {
+    struct Settings {
+        static let title: String = "Settings"
+        static let myAccount: String = "My Account"
+        static let crowdMap: String = "Contribute to CrowdMap"
+        static let crowdMapDescription: String = "Data contributed to the CrowdMap is publicly available at aircasting.org"
+        static let backendSettings: String = "Backend settings"
+        static let settingsHelp: String = "Help"
+        static let hardwareDevelopers: String = "Hardware developers"
+        static let about: String = "About AirCasting"
+    }
+    
+    struct BackendSettings {
+        static let backendSettings: String = "Backend settings"
+        static let Ok: String = "Ok"
+        static let Cancel: String = "Cancel"
+    }
+    
+    struct MyAccountSettings {
+        static let title: String = "My account"
+        static let notLogged: String = "You aren’t currently logged in"
+        static let createAccount: String = "Create an account"
+        static let logIn: String = "Log In"
+    }
+    
+    struct SignOutSettings {
+        static let title: String = "My account"
+        static let Logged: String = "You are currently logged in as jan.krzempek@lunarlogic.io"
+        static let signOut: String = "Sign Out"
+    }
+}
