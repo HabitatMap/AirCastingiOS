fa99042ee67057ace617d231d82dc8d5ca1fad81
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-07-14 13:18:53 +0200

    added clear rectangle over button which expanded space (#74)
    * added rectangle clear over button which expanded space
    
    * changes to CR
    
    * changes to CR
    
    Co-authored-by: Jan Krzempek <jankrzempek@MacBook-Pro.local>

--- AirCasting/Dashboard/DashboardView.swift
+++ AirCasting/Dashboard/DashboardView.swift
@@ -20,7 +20,6 @@ struct DashboardView: View {
     var body: some View {
         VStack {
             AirSectionPickerView(selection: $selectedSection)
-
             if sessions.isEmpty {
                 EmptyDashboardView()
             } else {
--- AirCasting/SessionViews/SessionHeaderView.swift
+++ AirCasting/SessionViews/SessionHeaderView.swift
@@ -91,7 +91,12 @@ private extension SessionHeaderView {
                 Label("Delete session", systemImage: "xmark.circle")
             }
         } label: {
-            EditButtonView()
+            ZStack(alignment: .trailing) {
+                EditButtonView()
+                Rectangle()
+                    .frame(width: 30, height: 20, alignment: .trailing)
+                    .opacity(0.0001)
+            }
         }
     }
     
