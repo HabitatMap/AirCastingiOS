2ae6e3b7db8c3631e2b3d81b13e8a6086748cd74
Jan Krzempek <jankrzempek@MacBook-Pro.local>
2021-07-22 13:14:26 +0200

    changes

--- AirCasting/CreateSessionViews/ConnectingABViewModel.swift
+++ AirCasting/CreateSessionViews/ConnectingABViewModel.swift
@@ -5,22 +5,38 @@ import CoreBluetooth
 import Foundation
 
 protocol AirBeamConnector {
-    func performConnectingWithin10Second(peripheral: CBPeripheral, completion: @escaping () -> Void)
+    func performConnectionWithin10Second(peripheral: CBPeripheral, completion: @escaping () -> Void)
 }
 
-class ConnectingABViewModel: AirBeamConnector {
+protocol BluetoothConnector {
+    func connectWithDevice(peripheral: CBPeripheral)
+    func cancelConnectionWithDevice(peripheral: CBPeripheral)
+}
+
+class DefaultBluetoothConnector {
     var bluetoothManager: BluetoothManager
     
     init(bluetoothManager: BluetoothManager) {
         self.bluetoothManager = bluetoothManager
     }
-    
-    func performConnectingWithin10Second(peripheral: CBPeripheral, completion: @escaping () -> Void) {
+}
+
+class BluetoothAirBeamConnection: DefaultBluetoothConnector, BluetoothConnector {
+    func connectWithDevice(peripheral: CBPeripheral) {
         bluetoothManager.centralManager.connect(peripheral, options: nil)
+    }
+    func cancelConnectionWithDevice(peripheral: CBPeripheral) {
+        bluetoothManager.centralManager.cancelPeripheralConnection(peripheral)
+    }
+}
+
+class DefaultAirBeamConnector: BluetoothAirBeamConnection, AirBeamConnector {
+    func performConnectionWithin10Second(peripheral: CBPeripheral, completion: @escaping () -> Void) {
+      connectWithDevice(peripheral: peripheral)
         DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [self] in
             if peripheral.state == .connecting {
                 Log.info("Connecting to bluetooth device failed")
-                self.bluetoothManager.centralManager.cancelPeripheralConnection(peripheral)
+                cancelConnectionWithDevice(peripheral: peripheral)
                 completion()
             }
         }
