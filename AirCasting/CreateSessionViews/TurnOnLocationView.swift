154f0c5dde536ac553e7922ff2a4c5f437519171
Jan Krzempek <jankrzempek@MacBook-Pro.local>
2021-07-26 17:49:48 +0200

    changes

---
+++ AirCasting/CreateSessionViews/TurnOnLocationView.swift
@@ -0,0 +1,16 @@
+// Created by Lunar on 26/07/2021.
+//
+
+import SwiftUI
+
+struct TurnOnLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TurnOnLocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        TurnOnLocationView()
+    }
+}
