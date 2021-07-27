8d32cad5cff19eb16f0e00de27214a83fe4e6864
Monika Śmiałko <monika.smialko@gmail.com>
2021-07-27 14:46:07 +0200

    Fix threshold in ABMeasurementsView

--- AirCasting/SessionViews/ABMeasurementsView.swift
+++ AirCasting/SessionViews/ABMeasurementsView.swift
@@ -39,12 +39,14 @@ struct ABMeasurementsView: View {
                         .padding(.horizontal)
                     HStack {
                         Group {
-                            ForEach(streams, id : \.self) {
-                                SingleMeasurementView(stream: $0,
-                                                      value: session.isDormant ? $0.averageValue : ($0.latestValue ?? 0),
-                                                      threshold: threshold,
-                                                      selectedStream: _selectedStream,
-                                                      measurementPresentationStyle: measurementPresentationStyle)
+                            ForEach(streams, id : \.self) { stream in
+                                if let threshold = thresholds.threshold(for: stream) {
+                                    SingleMeasurementView(stream: stream,
+                                                          value: stream.latestValue ?? 0,
+                                                          threshold: threshold,
+                                                          selectedStream: _selectedStream,
+                                                          measurementPresentationStyle: measurementPresentationStyle)
+                                }
                             }
                         }
                         .frame(maxWidth: .infinity)
