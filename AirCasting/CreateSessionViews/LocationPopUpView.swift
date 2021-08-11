2989b1d7d983a9c07bd15db14986d4cc42e4ac13
Jan Krzempek <jankrzempek@MacBook-Pro.local>
2021-08-11 12:46:44 +0200

    changes

--- AirCasting/CreateSessionViews/LocationPopUpView.swift
+++ AirCasting/CreateSessionViews/LocationPopUpView.swift
@@ -2,10 +2,27 @@
 //
 
 import SwiftUI
+import AirCastingStyling
 
 struct LocationPopUpView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            titleLabel
+            GoogleMapView(pathPoints: [])
+                .padding()
+            Button(action: {
+            }, label: {
+                Text(Strings.ConfirmCreatingSessionView.startRecording)
+                    .bold()
+            })
+            .buttonStyle(BlueButtonStyle())
+        }.padding()
+    }
+    
+    var titleLabel: some View {
+        Text("Search the adress and ajust the marker to indicate an exact placement of Your AirBeam")
+            .font(Font.moderate(size: 24, weight: .bold))
+            .foregroundColor(.darkBlue)
     }
 }
 
