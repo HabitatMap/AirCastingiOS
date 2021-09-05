2be6ec51f43189664cfd1d305cd6b3950e2828ba
Pawel Gil <pawel.gil@me.com>
2021-09-05 22:46:14 +0200

    Removed redundant or commented out code

--- AirCasting/CreateSessionViews/ConfirmCreatingSessionView.swift
+++ AirCasting/CreateSessionViews/ConfirmCreatingSessionView.swift
@@ -117,7 +117,6 @@ struct ConfirmCreatingSessionView: View {
                         }
                         isActive = false
                     }
-                    isActive = false
                 }
             }, label: {
                 Text(Strings.ConfirmCreatingSessionView.startRecording)
