974e7cfbb2d9e0a9945f60ad24319b1d9d8fec55
Jan Krzempek <jankrzempek@MacBook-Pro.local>
2021-09-20 17:14:26 +0200

    removed unused code

--- AirCasting/Utils/TimeConversion.swift
+++ AirCasting/Utils/TimeConversion.swift
@@ -11,31 +11,4 @@ final class TimeConverter {
         let dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: Locale.current)!
         return dateFormat.firstIndex(of: "a") == nil
     }
-
-    static func timeConversion24(time12: String) -> String {
-        let dateAsString = time12
-        df.locale = Locale(identifier: "en_US_POSIX")
-        df.dateFormat = "h:mm a"
-        
-        let date = df.date(from: dateAsString)
-        df.dateFormat = "HH:mm"
-        
-        let time24 = df.string(from: date!)
-        return time24
-    }
-    
-    static func swapDaysAndMonths(date: String) -> String {
-        var components = date.components(separatedBy: "/")
-        let days = components.first
-        components[0] = components[1]
-        components[1] = days!
-        var fullDate = ""
-        for component in components {
-            fullDate.append(component)
-            if component != components.last {
-                fullDate.append("/")
-            }
-        }
-        return fullDate
-    }
 }
