91b72cabe5ceee14185db8d18e3833e5234c817c
Jan Krzempek <jankrzempek@MacBook-Pro.local>
2021-09-22 09:32:22 +0200

    Monika CR suggestions

--- AirCasting/CreateSessionViews/ChooseCustomLocationView.swift
+++ AirCasting/CreateSessionViews/ChooseCustomLocationView.swift
@@ -7,7 +7,7 @@ import SwiftUI
 struct ChooseCustomLocationView: View {
     @State private var isConfirmCreatingSessionActive: Bool = false
     @State private var location = ""
-    @State var placePickerDismissed: Bool = false
+    @State var placePickerPresented: Bool = false
     @State var isLocationPopupPresented = false
     @Binding var creatingSessionFlowContinues: Bool
     @Binding var sessionName: String
@@ -30,16 +30,17 @@ struct ChooseCustomLocationView: View {
         }
         .background(confirmCreatingSessionLink)
         .sheet(isPresented: $isLocationPopupPresented) {
-            PlacePicker(placePickerDismissed: $placePickerDismissed, address: $location)
+            PlacePicker(placePickerDismissed: $placePickerPresented, address: $location)
         }
-        .onChange(of: placePickerDismissed, perform: { value in
-            placePickerDismissed ? (placePickerDismissed = false) : nil
+        .onChange(of: placePickerPresented, perform: { value in
+            // The reason for this is to prevent map from multiple times refreshing after first map update
+            placePickerPresented ? (placePickerPresented = false) : nil
         })
         .padding()
     }
 
     var mapGoogle: some View {
-        GoogleMapView(pathPoints: [], placePickerDismissed: $placePickerDismissed)
+        GoogleMapView(pathPoints: [], placePickerDismissed: $placePickerPresented)
     }
 
     var dot: some View {
--- AirCasting/Map/GoogleMapView.swift
+++ AirCasting/Map/GoogleMapView.swift
@@ -207,7 +207,7 @@ struct GoogleMapView_Previews: PreviewProvider {
                                              measurementTime: .distantPast,
                                              measurement: 30)],
                       threshold: .mock,
-                      placePickerDismissed: .constant(false))
+                      placePickerPresented: .constant(false))
             .padding()
     }
 }
