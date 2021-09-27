c0e0fccea2aee9076546a6ac3ea191be7625f846
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-09-27 18:47:50 +0200

    Handle right time graph (#262)
    * added custom times below graph
    
    * changes to the graph
    
    * improvements
    
    Co-authored-by: Jan Krzempek <jankrzempek@MacBook-Pro.local>
    Co-authored-by: Jan Krzempek <jankrzempek@Tomaszs-MBP.localdomain>

--- AirCasting/Graph/GraphView.swift
+++ AirCasting/Graph/GraphView.swift
@@ -36,35 +36,92 @@ struct GraphView<StatsViewModelType>: View where StatsViewModelType: StatisticsC
                                thresholds: thresholds,
                                measurementPresentationStyle: .showValues)
                 .padding(.horizontal)
-            if let threshold = thresholds.threshold(for: selectedStream) {
-                ZStack(alignment: .topLeading) {
-                    if let selectedStream = selectedStream {
-                        Graph(stream: selectedStream,
-                              thresholds: threshold,
-                              isAutozoomEnabled: session.type == .mobile).onDateRangeChange { [weak graphStatsDataSource, weak statsContainerViewModel] range in
-                                graphStatsDataSource?.dateRange = range
-                                statsContainerViewModel?.adjustForNewData()
-                              }
-                        StatisticsContainerView(statsContainerViewModel: statsContainerViewModel,
-                                                threshold: threshold)
-                    }
+           
+            if procceding(session: session) {
+                    if let threshold = thresholds.threshold(for: selectedStream) {
+                        ZStack(alignment: .topLeading) {
+                            if let selectedStream = selectedStream {
+                                Graph(stream: selectedStream,
+                                      thresholds: threshold,
+                                      isAutozoomEnabled: session.type == .mobile).onDateRangeChange { [weak graphStatsDataSource, weak statsContainerViewModel] range in
+                                        graphStatsDataSource?.dateRange = range
+                                        statsContainerViewModel?.adjustForNewData()
+                                      }
+                                StatisticsContainerView(statsContainerViewModel: statsContainerViewModel,
+                                                        threshold: threshold)
+                            }
 
-                }
-                NavigationLink(destination: HeatmapSettingsView(changedThresholdValues: threshold.rawThresholdsBinding)) {
-                    EditButtonView()
-                }
-                .padding(.horizontal)
-                .padding(.top)
-                
-                ThresholdsSliderView(threshold: threshold)
-                    .padding()
-                    // Fixes labels covered by tabbar
-                    .padding(.bottom)
+                        }
+                        HStack() {
+                            startTimeText
+                            Spacer()
+                            endTimeText
+                        }
+                        .foregroundColor(.aircastingGray)
+                        .padding(.horizontal, 5)
+                        NavigationLink(destination: HeatmapSettingsView(changedThresholdValues: threshold.rawThresholdsBinding)) {
+                            EditButtonView()
+                        }
+                        .padding(.horizontal)
+                        .padding(.top)
+                        
+                        ThresholdsSliderView(threshold: threshold)
+                            .padding()
+                            // Fixes labels covered by tabbar
+                            .padding(.bottom)
+                    }
             }
             Spacer()
         }
         .navigationBarTitleDisplayMode(.inline)
     }
+    
+    var startTimeText: some View {
+        startTime()
+    }
+    
+    var endTimeText: some View {
+        endTime()
+    }
+    
+    func procceding(session: SessionEntity) -> Bool {
+        var showGraph = false
+        guard session.allStreams != nil else { return false }
+        (session.allStreams!.forEach({ stream in
+            (stream.allMeasurements != []) ? (showGraph = true) : (showGraph = false)
+        }))
+        if showGraph {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func startTime() -> Text {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US")
+        if !(session.isMobile && session.isActive) {
+            formatter.timeZone =  TimeZone.init(abbreviation: "UTC")
+        }
+            formatter.dateFormat = "HH:mm"
+            guard let start = session.startTime else { return Text("") }
+     
+            let string = formatter.string(from: start)
+            return Text(string)
+        }
+    
+    func endTime() -> Text {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US")
+        if !(session.isMobile && session.isActive) {
+            formatter.timeZone =  TimeZone.init(abbreviation: "UTC")
+        }
+            formatter.dateFormat = "HH:mm"
+            let end = session.endTime ?? Date()
+     
+            let string = formatter.string(from: end)
+            return Text(string)
+        }
 }
 
 #if DEBUG
