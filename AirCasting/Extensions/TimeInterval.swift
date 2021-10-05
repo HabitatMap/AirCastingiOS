c656d4ba00653188c4ca3ab08738c47fc36a16a9
Anna Olak <anna.olak1901@gmail.com>
2021-10-05 13:10:11 +0200

    Make small changes to chartViewModel

---
+++ AirCasting/Extensions/TimeInterval.swift
@@ -0,0 +1,10 @@
+// Created by Lunar on 05/10/2021.
+//
+
+import Foundation
+
+extension TimeInterval {
+    static let minute: TimeInterval = 60
+    static let hour: TimeInterval = minute * 60
+    static let day: TimeInterval = hour * 24
+}
