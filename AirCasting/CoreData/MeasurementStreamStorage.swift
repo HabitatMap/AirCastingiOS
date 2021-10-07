acb6757630943c360a60fab93fc40c850ad79041
Jan Krzempek <jankrzempek@MacBook-Pro-2.lan>
2021-10-07 14:25:59 +0200

    changed measurement time to proper one we get from web

--- AirCasting/CoreData/MeasurementStreamStorage.swift
+++ AirCasting/CoreData/MeasurementStreamStorage.swift
@@ -24,8 +24,8 @@ protocol MeasurementStreamStorageContextUpdate {
 }
 
 extension HiddenCoreDataMeasurementStreamStorage {
-    func addMeasurementValue(_ value: Double, at location: CLLocationCoordinate2D? = nil, toStreamWithID id: MeasurementStreamLocalID) throws {
-        try addMeasurement(Measurement(time: Date(), value: value, location: location), toStreamWithID: id)
+    func addMeasurementValue(_ value: Double, at location: CLLocationCoordinate2D? = nil, toStreamWithID id: MeasurementStreamLocalID, on time: Date? = Date()) throws {
+        try addMeasurement(Measurement(time: time!, value: value, location: location), toStreamWithID: id)
     }
 }
 
