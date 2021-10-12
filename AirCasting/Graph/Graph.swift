a7b6c0394d7f444ca1b95815e756ad03051faca5
Jan Krzempek <jankrzempek@MacBook-Pro-2.local>
2021-10-12 14:42:51 +0200

    added range for detecting midnight lines

--- AirCasting/Graph/Graph.swift
+++ AirCasting/Graph/Graph.swift
@@ -78,10 +78,11 @@ struct Graph: UIViewRepresentable {
         }
     }
     
-    func getMidnightsPoints(startingDate: Date) -> [Double] {
-        let day = Date()
-        // [SMALL HACK] - By adding a day to the current Date we are ensuring
+    func getMidnightsPoints(startingDate: Date, endingDate: Date) -> [Double] {
+        let day = endingDate
+        // [SMALL HACK] - By adding a day to the lastMeasurement Date we are ensuring
         // that session which is currently recording will be able to show midnight line
+        // or on any other session the midnight line will be considered well
         let nextDay = Calendar.current.date(byAdding: .day, value: 1, to: day)!
         var midnight = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: nextDay)!
         let components = Calendar.current.dateComponents([.hour, .minute, .second], from: midnight)
@@ -103,7 +104,7 @@ struct Graph: UIViewRepresentable {
     }
     
     func getLimitLines() -> [ChartLimitLine] {
-        let points = getMidnightsPoints(startingDate: stream.allMeasurements?.last?.time ?? Date())
+        let points = getMidnightsPoints(startingDate: stream.allMeasurements?.first?.time ?? Date(), endingDate: stream.allMeasurements?.last?.time ?? Date())
         
         return points.map { point in
             let line = ChartLimitLine(limit: point)
