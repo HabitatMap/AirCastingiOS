a3b84c1aa637a02bfb7291317d5c008748bfab61
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-10-12 20:33:11 +0200

    Fix - midnight lines on graph (#298)
    * added day to current day
    
    * Ania and Monika changes
    
    * added range for detecting midnight lines
    
    Co-authored-by: Jan Krzempek <jankrzempek@MacBook-Pro-2.lan>
    Co-authored-by: Jan Krzempek <jankrzempek@MacBook-Pro-2.local>

--- AirCasting/Graph/Graph.swift
+++ AirCasting/Graph/Graph.swift
@@ -84,29 +84,33 @@ struct Graph: UIViewRepresentable {
         }
     }
     
-    func getMidnightsPoints(startingDate: Date) -> [Double] {
-        let now = Date()
-        var midnight = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: now)!
+    func getMidnightsPoints(startingDate: Date, endingDate: Date) -> [Double] {
+        let day = endingDate
+        // [SMALL HACK] - By adding a day to the lastMeasurement Date we are ensuring
+        // that session which is currently recording will be able to show midnight line
+        // or on any other session the midnight line will be considered well
+        let nextDay = Calendar.current.date(byAdding: .day, value: 1, to: day)!
+        var midnight = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: nextDay)!
         let components = Calendar.current.dateComponents([.hour, .minute, .second], from: midnight)
         let firstMeasurementDate = startingDate
         var midnightPoints: [Double] = []
         
         while (midnight > firstMeasurementDate) {
-            let previus = Calendar.current.nextDate(after: midnight,
+            let previous = Calendar.current.nextDate(after: midnight,
                                                     matching: components,
                                                     matchingPolicy: Calendar.MatchingPolicy.previousTimePreservingSmallerComponents,
                                                     repeatedTimePolicy: Calendar.RepeatedTimePolicy.first,
                                                     direction: .backward)
-            if let midnightPoint = previus?.timeIntervalSince1970 {
+            if let midnightPoint = previous?.timeIntervalSince1970 {
                 midnightPoints.append(midnightPoint)
             }
-            midnight = previus!
+            midnight = previous!
         }
         return midnightPoints
     }
     
     func getLimitLines() -> [ChartLimitLine] {
-        let points = getMidnightsPoints(startingDate: stream.allMeasurements?.first?.time ?? Date())
+        let points = getMidnightsPoints(startingDate: stream.allMeasurements?.first?.time ?? Date(), endingDate: stream.allMeasurements?.last?.time ?? Date())
         
         return points.map { point in
             let line = ChartLimitLine(limit: point)
