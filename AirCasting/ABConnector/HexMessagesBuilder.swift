03e4aca9247f1276737bb9d593862bb16513e0f8
Maria <mturnau@users.noreply.github.com>
2021-10-15 11:17:19 +0200

    Removed unnecessary brackets and changed to compound operator

--- AirCasting/ABConnector/HexMessagesBuilder.swift
+++ AirCasting/ABConnector/HexMessagesBuilder.swift
@@ -15,8 +15,8 @@ struct HexMessagesBuilder {
             let timeZone = calendar.timeZone
             var offset = timeZone.secondsFromGMT()
             // We need to send *standard* offset, ignoring DST because AirBeam is calculating DST anyway
-            if (timeZone.isDaylightSavingTime()) {
-                offset = offset - Int(timeZone.daylightSavingTimeOffset())
+            if timeZone.isDaylightSavingTime() {
+                offset -= Int(timeZone.daylightSavingTimeOffset())
             }
             let hoursOffset = Int(offset / 3600)
             return hoursOffset
