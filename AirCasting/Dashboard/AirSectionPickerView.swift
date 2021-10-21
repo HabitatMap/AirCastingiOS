c457fa0a2fe54b83d38be1ae8736d6f1a5ae046c
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-10-21 12:07:59 +0100

    view now properly scroll to following (#339)
    * view now properly scrool to following
    
    * added animation
    
    Co-authored-by: Jan Krzempek <jankrzempek@MacBook-Pro-2.lan>

--- AirCasting/Dashboard/AirSectionPickerView.swift
+++ AirCasting/Dashboard/AirSectionPickerView.swift
@@ -14,14 +14,18 @@ struct AirSectionPickerView: View {
                     ForEach(SelectedSection.allCases, id: \.self) { section in
                         Button(section.localizedString) {
                             withAnimation(.easeInOut(duration: 0.1)) {
-                                if section == .mobileDormant {
+                                if section == .mobileDormant || section == .fixed {
                                     scrollReader.scrollTo(SelectedSection.fixed)
                                 } else if section == .mobileActive {
                                     scrollReader.scrollTo(SelectedSection.following)
                                 }
                                 selection = section
                             }
-                        }
+                        }.onChange(of: selection, perform: { section in
+                            withAnimation(.easeInOut(duration: 0.1)) {
+                                section == .following ? scrollReader.scrollTo(SelectedSection.following) : nil
+                            }
+                        })
                         .buttonStyle(PickerButtonStyle(isSelected: section == selection))
                         .id(section)
                     }
