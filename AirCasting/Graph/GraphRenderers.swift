bbc0a8bed868d09f4537e4fc8c720eefc0211d6e
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-10-22 10:03:33 +0100

    graph fixed by changing Fonts to UIFont (#347)

--- AirCasting/Graph/GraphRenderers.swift
+++ AirCasting/Graph/GraphRenderers.swift
@@ -24,7 +24,7 @@ class TimeAxisRenderer: XAxisRenderer {
                   x: minPxX + 10,
                   y: bottomPxY,
                   attributes: [.foregroundColor : UIColor.aircastingGray,
-                               .font: Fonts.muliHeading3],
+                               .font: Fonts.muliHeadingUIFont1],
                   constrainedToSize: .zero,
                   anchor: .zero,
                   angleRadians: 0)
@@ -37,7 +37,7 @@ class TimeAxisRenderer: XAxisRenderer {
                   x: maxPxX - 50,
                   y: bottomPxY,
                   attributes: [.foregroundColor : UIColor.aircastingGray,
-                               .font: Fonts.muliHeading3],
+                               .font: Fonts.muliHeadingUIFont1],
                   constrainedToSize: .zero,
                   anchor: .zero,
                   angleRadians: 0)
