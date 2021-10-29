a99fc3fc3516e4e542ecbcba3f8ed86d1b2af56f
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2021-10-29 16:35:40 +0100

    added delay to start recording after intteruption (#371)
    * added delay to start recording after intteruption
    
    * refactor
    
    * added warning
    
    * fix
    
    Co-authored-by: Jan Krzempek <jankrzempek@Jans-MBP.lan>

--- AirCasting/MicrophoneSession/MicrophoneManager.swift
+++ AirCasting/MicrophoneSession/MicrophoneManager.swift
@@ -97,9 +97,10 @@ extension MicrophoneManager: AVAudioRecorderDelegate {
     func audioRecorderEndInterruption(_ recorder: AVAudioRecorder, withOptions flags: Int) {
         Log.info("audio recorder end interruption")
         if !recorder.isRecording {
-            recorder.record()
+            #warning("5 second added as a [for now] implementation - find beeter way")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) { recorder.record() }
         }
-        levelTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTick), userInfo: nil, repeats: true)
+        self.levelTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerTick), userInfo: nil, repeats: true)
     }
 
     func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
@@ -111,7 +112,9 @@ private extension MicrophoneManager {
     func sampleMeasurement() {
         recorder.updateMeters()
         let power = recorder.averagePower(forChannel: 0)
-        let decibels = Double(power + 90.0)
+        var decibels = Double(power + 90.0)
+        (decibels < 0) ? decibels = 0 : nil
+        // 117 lines ensure that we won't get something like -70 etc.
         let location = obtainCurrentLocation()
         
         measurementStreamStorage.accessStorage { storage in
@@ -159,4 +162,3 @@ private extension MicrophoneManager {
         case permissionNotGranted
     }
 }
-
