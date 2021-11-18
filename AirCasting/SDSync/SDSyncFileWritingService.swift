0c0ea9456fe08b2f6d78363b45fde10e6af33718
Anna Olak <anna.olak1901@gmail.com>
2021-11-18 22:23:37 +0100

    Update SDSyncFileWritingService.swift

--- AirCasting/SDSync/SDSyncFileWritingService.swift
+++ AirCasting/SDSync/SDSyncFileWritingService.swift
@@ -4,6 +4,14 @@
 import Foundation
 
 struct SDSyncFileWritingService: SDSyncFileWriter {
+    func finishAndSave() {
+        //
+    }
+    
+    func finishAndRemoveFiles() {
+        //
+    }
+    
     func writeToFile(data: String, sessionType: SDCardSessionType) {
         let dataEntries = data.components(separatedBy: "\r\n").filter { !$0.trimmingCharacters(in: ["\n"]).isEmpty }
         Log.info("## Data entries: \(dataEntries)")
