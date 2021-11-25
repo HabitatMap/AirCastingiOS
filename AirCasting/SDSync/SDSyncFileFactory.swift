712966027c75329f62d92b9e22f8996243b8d85b
Anna Olak <anna.olak1901@gmail.com>
2021-11-25 15:34:59 +0100

    Update SDSyncFileFactory.swift

--- AirCasting/SDSync/SDSyncFileFactory.swift
+++ AirCasting/SDSync/SDSyncFileFactory.swift
@@ -3,12 +3,8 @@
 
 import Foundation
 
-
+//TODO: I'm leaving this file just as an information about the file structure. It will changed when the implementation of saving data to db will be added
 class SDCardCSVFileFactory {
-    private let DIR_NAME = "sync"
-    private let MOBILE_FILE_NAME = "mobile.csv"
-    private let FIXED_FILE_NAME = "fixed.csv"
-
     enum Header {
         case index
         case uuid
@@ -24,25 +20,4 @@ class SDCardCSVFileFactory {
         case pm2_5
         case pm10
     }
-
-//    func getMobileFile() -> File {
-//        return getFile(MOBILE_FILE_NAME)
-//    }
-//
-//    func getFixedFile() -> File {
-//        return getFile(FIXED_FILE_NAME)
-//    }
-//
-//    func getFile(stepType: SDCardReader.StepType) -> File {
-//        return when(stepType) {
-//            SDCardReader.StepType.MOBILE -> getMobileFile()
-//            SDCardReader.StepType.FIXED_WIFI -> getFixedFile()
-//            SDCardReader.StepType.FIXED_CELLULAR -> getFixedFile()
-//        }
-//    }
-//
-//    private func getFile(fileName: String): File {
-//        val dir = mContext.getExternalFilesDir(DIR_NAME)
-//        return File(dir, fileName)
-//    }
 }
