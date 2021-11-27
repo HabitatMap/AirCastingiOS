3bca6509b1719e0bc4a46f7cd8e1a7e8b0a5bfca
Pawel Gil <pawel.gil@me.com>
2021-11-27 18:01:49 +0100

    Firebase config turned on

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -9,11 +9,15 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-        )
+        // NOTE: We don't ask user for the remote notifications permissions since we're only using APNS for
+        // the firebase config change notifications (silent push notifications - no permission is needed for
+        // this kind). If you want to add non-silet notifications, uncomment:
+        //
+        // let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        // UNUserNotificationCenter.current().requestAuthorization(
+        //     options: authOptions,
+        //     completionHandler: { _, _ in }
+        // )
         application.registerForRemoteNotifications()
         return true
     }
