40f4924ab5b6833d9ac372393813b1e4dd8caeb1
pawelgil <pawelgil@users.noreply.github.com>
2021-12-07 18:25:57 +0100

    Sd sync progress indication (#476)
    * New parts of BluetoothManager moved to a BluetoothCommunicator protocol
    
    * Progress added to SDSyncController + minor changes to sd sync interfaces
    
    * Synchronization progress hooked to VM
    
    * Display progress in view

--- AirCasting/SDSync/ViewsFlow/SDSyncingAB/SDSyncViewModel.swift
+++ AirCasting/SDSync/ViewsFlow/SDSyncingAB/SDSyncViewModel.swift
@@ -4,30 +4,43 @@
 import Combine
 import CoreBluetooth
 
+struct SDSyncProgressViewModel {
+    let title: String
+    let current: String
+    let total: String
+
+    func progressLabel() -> String {
+        "Syncing " + title + ": " + "\(current)/\(total)"
+    }
+}
+
 protocol SDSyncViewModel: ObservableObject {
     var shouldDismiss: Published<Bool>.Publisher { get }
     var isSyncCompleted: Published<Bool>.Publisher { get }
     var presentNextScreen: Bool { get set }
     var presentAlert: Bool { get set }
+    var progress: Published<SDSyncProgressViewModel?>.Publisher { get }
     func connectToAirBeamAndSync()
 }
 
 class SDSyncViewModelDefault: SDSyncViewModel, ObservableObject {
-    
+
     var shouldDismiss: Published<Bool>.Publisher { $shouldDismissValue }
     var isSyncCompleted: Published<Bool>.Publisher { $isSyncCompletedValue }
-    
+    var progress: Published<SDSyncProgressViewModel?>.Publisher { $progressValue }
+
     @Published private var shouldDismissValue: Bool = false
     @Published private var isSyncCompletedValue: Bool = false
+    @Published private var progressValue: SDSyncProgressViewModel?
     @Published var presentNextScreen: Bool = false
     @Published var presentAlert: Bool = false
-    
+
     private let peripheral: CBPeripheral
     private let airBeamConnectionController: AirBeamConnectionController
     private let sdSyncController: SDSyncController
     private let userAuthenticationSession: UserAuthenticationSession
     private let sessionContext: CreateSessionContext
-    
+
     init(airBeamConnectionController: AirBeamConnectionController,
          sdSyncController: SDSyncController,
          userAuthenticationSession: UserAuthenticationSession,
@@ -39,36 +52,52 @@ class SDSyncViewModelDefault: SDSyncViewModel, ObservableObject {
         self.userAuthenticationSession = userAuthenticationSession
         self.sessionContext = sessionContext
     }
-    
+
     func connectToAirBeamAndSync() {
         self.airBeamConnectionController.connectToAirBeam(peripheral: peripheral) { success in
             guard success else { return }
             self.configureABforSync()
-            self.sdSyncController.syncFromAirbeam(self.peripheral) { result in
+            self.sdSyncController.syncFromAirbeam(self.peripheral, progress: { [weak self] newProgress in
+                guard let self = self else { return }
+                DispatchQueue.main.async {
+                    let sessionType = self.stringForSessionType(newProgress.sessionType)
+                    self.progressValue = .init(title: sessionType, current: String(newProgress.progress.received), total: String(newProgress.progress.expected))
+                }
+            }, completion: { [weak self] result in
                 //TODO: SD card should be cleared only if the files are not corrupted
+                guard let self = self else { return }
                 result ? self.clearSDCard() : nil
                 self.disconnectAirBeam()
                 DispatchQueue.main.async {
                     self.isSyncCompletedValue = result
                     self.shouldDismissValue = !result
                 }
-            }
+            })
         }
     }
-    
+
     private func configureABforSync() {
         let configurator = AirBeam3Configurator(userAuthenticationSession: self.userAuthenticationSession,
                                                 peripheral: self.peripheral)
         configurator.configureSDSync()
     }
-    
+
     private func clearSDCard() {
         let configurator = AirBeam3Configurator(userAuthenticationSession: self.userAuthenticationSession,
                                                 peripheral: self.peripheral)
         configurator.clearSDCard()
     }
-    
+
     private func disconnectAirBeam() {
         airBeamConnectionController.disconnectAirBeam(peripheral: peripheral)
+    }
+
+    private func stringForSessionType(_ sessionType: SDCardSessionType) -> String {
+        //TODO: Correct string value and move to strings
+        switch sessionType {
+        case .cellular: return "Cellular"
+        case .fixed: return "Fixed"
+        case .mobile: return "Mobile"
+        }
     }
 }
