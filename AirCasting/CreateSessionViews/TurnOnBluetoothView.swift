ac213b47cd8357e3e48934033b014745279c2a8f
Anna Olak <anna.olak1901@gmail.com>
2021-12-09 18:06:26 +0100

    Fixes after merging develop

--- AirCasting/CreateSessionViews/TurnOnBluetoothView.swift
+++ AirCasting/CreateSessionViews/TurnOnBluetoothView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct TurnOnBluetoothView: View {
     @State private var isPowerABLinkActive = false
     @State private var presentRestartScreen = false
+    @State private var presentUnplugScreen = false
     @EnvironmentObject var settingsRedirection: DefaultSettingsRedirection
     @EnvironmentObject var bluetoothManager: BluetoothManager
     @Binding var creatingSessionFlowContinues: Bool
@@ -44,11 +45,18 @@ struct TurnOnBluetoothView: View {
             )
            NavigationLink(
                 destination: UnplugABView(viewModel: UnplugABViewModelDefault(urlProvider: urlProvider, isSDClearProcess: isSDClearProcess), creatingSessionFlowContinues: $creatingSessionFlowContinues),
-                isActive: $presentRestartScreen,
+                isActive: $presentUnplugScreen,
                 label: {
                     EmptyView()
-                })
-            }
+                }
+           )
+            NavigationLink(
+                 destination: SDRestartABView(viewModel: SDRestartABViewModelDefault(urlProvider: urlProvider, isSDClearProcess: isSDClearProcess), creatingSessionFlowContinues: $creatingSessionFlowContinues),
+                 isActive: $presentRestartScreen,
+                 label: {
+                     EmptyView()
+                 })
+             }
         )
         .onAppear(perform: {
             if CBCentralManager.authorization != .allowedAlways {
@@ -78,7 +86,11 @@ struct TurnOnBluetoothView: View {
             } else if bluetoothManager.centralManager.state != .poweredOn {
                 settingsRedirection.goToBluetoothAuthSettings()
             } else {
-                sdSyncContinues ? presentRestartScreen.toggle() : isPowerABLinkActive.toggle()
+                if isSDClearProcess {
+                    presentRestartScreen.toggle()
+                } else {
+                    sdSyncContinues ? presentUnplugScreen.toggle() : isPowerABLinkActive.toggle()
+                }
             }
         }, label: {
             Text(Strings.TurnOnBluetoothView.continueButton)
--- AirCasting/SDSync/ViewsFlow/SDSyncUnplugAB/UnplugABViewModel.swift
+++ AirCasting/SDSync/ViewsFlow/SDSyncUnplugAB/UnplugABViewModel.swift
@@ -6,6 +6,7 @@ import Foundation
 protocol UnplugABViewModel: ObservableObject {
     var presentNextScreen: Bool { get }
     var urlProvider: BaseURLProvider { get }
+    var isSDClearProcess: Bool { get set }
     func continueButtonTapped()
 }
 
@@ -13,9 +14,11 @@ class UnplugABViewModelDefault: UnplugABViewModel, ObservableObject {
     
     @Published var presentNextScreen: Bool = false
     let urlProvider: BaseURLProvider
+    var isSDClearProcess: Bool
     
-    init(urlProvider: BaseURLProvider) {
+    init(urlProvider: BaseURLProvider, isSDClearProcess: Bool) {
         self.urlProvider = urlProvider
+        self.isSDClearProcess = isSDClearProcess
     }
     
     func continueButtonTapped() {
@@ -27,6 +30,7 @@ class UnplugABViewModelDummy: UnplugABViewModel, ObservableObject {
     
     @Published var presentNextScreen: Bool = false
     let urlProvider: BaseURLProvider
+    var isSDClearProcess = false
     
     init() {
         self.urlProvider = DummyURLProvider()
