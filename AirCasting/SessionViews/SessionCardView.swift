ca2004af470b6459560304dcc64b27b43b701f2a
Jan Krzempek <jankrzempek@Jans-MBP.lan>
2021-12-15 09:54:57 +0000

    fix

--- AirCasting/SessionViews/SessionCardView.swift
+++ AirCasting/SessionViews/SessionCardView.swift
@@ -240,7 +240,7 @@ private extension SessionCardView {
             Spacer()
             !session.isIndoor ? mapButton.padding(.trailing, 10) : nil
             graphButton
-        }
+        }.padding(.top, 10)
         .buttonStyle(GrayButtonStyle())
     }
 
