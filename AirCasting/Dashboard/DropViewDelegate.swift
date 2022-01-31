02ee778db6338830eefb207fc992bdf832ab8b27
Anna Olak <anna.olak1901@gmail.com>
2022-01-31 19:36:56 +0100

    Improve the dragging and droping

--- AirCasting/Dashboard/DropViewDelegate.swift
+++ AirCasting/Dashboard/DropViewDelegate.swift
@@ -5,29 +5,48 @@ import SwiftUI
 
 struct DropViewDelegate: DropDelegate {
     
-    var session: SessionEntity
-    var sessionsData: ReorderingDashboardViewModel
-    
-    func performDrop(info: DropInfo) -> Bool {
-        return true
-    }
+    let session: SessionEntity
+    @Binding var currentSession: SessionEntity?
+    @Binding var sessions: [SessionEntity]
+    @Binding var changedView: Bool
     
     func dropEntered(info: DropInfo) {
-        print(session.name)
-        let fromIndex = sessionsData.sessions.firstIndex { (session) -> Bool in
-            return session.uuid == sessionsData.currentSession?.uuid
+        changedView = true
+        
+        let fromIndex = sessions.firstIndex { (session) -> Bool in
+            return session.uuid == currentSession?.uuid
         } ?? 0
         
-        let toIndex = sessionsData.sessions.firstIndex { (session) -> Bool in
+        let toIndex = sessions.firstIndex { (session) -> Bool in
             return session.uuid == self.session.uuid
         } ?? 0
         
         if fromIndex != toIndex {
             withAnimation(.default) {
-                let fromPage = sessionsData.sessions[fromIndex]
-                sessionsData.sessions[fromIndex] = sessionsData.sessions[toIndex]
-                sessionsData.sessions[toIndex] = fromPage
+                let fromPage = sessions[fromIndex]
+                sessions[fromIndex] = sessions[toIndex]
+                sessions[toIndex] = fromPage
             }
         }
+    }
+    
+    func dropUpdated(info: DropInfo) -> DropProposal? {
+            return DropProposal(operation: .move)
+        }
+    
+    
+    func performDrop(info: DropInfo) -> Bool {
+        currentSession = nil
+        return true
+    }
+}
+
+struct DropOutsideOfGridDelegate: DropDelegate {
+    
+    @Binding var currentSession: SessionEntity?
+    
+    func performDrop(info: DropInfo) -> Bool {
+        currentSession = nil
+        return true
     }
 }
--- AirCasting/Dashboard/ReorderingDashboard.swift
+++ AirCasting/Dashboard/ReorderingDashboard.swift
@@ -6,44 +6,36 @@ import SwiftUI
 struct ReorderingDashboard: View {
     
     @ObservedObject var viewModel: ReorderingDashboardViewModel
+    @State private var changedView: Bool = false
     var thresholds: [SensorThreshold]
     let measurementStreamStorage: MeasurementStreamStorage
     let urlProvider: BaseURLProvider
     
     let columns = [GridItem(.flexible())]
     
-//    var body: some View {
-//        ScrollView {
-//            LazyVGrid(columns: columns) {
-//                ForEach(viewModel.sessions) { session in
-//                    ReoredringSessionCard(session: session, thresholds: thresholds, measurementStreamStorage: measurementStreamStorage, urlProvider: urlProvider)
-//                        .onDrag({
-//                            viewModel.currentSession = session
-//                            return NSItemProvider(contentsOf: URL(string: session.urlLocation ?? "")!)!
-//                        })
-//                        .onDrop(of: [.url], delegate: DropViewDelegate(session: session, sessionsData: viewModel))
-//                }
-//            }
-//            .padding()
-//        }
-//    }
-    
-//    var body: some View {
-//            List {
-//                    ForEach(viewModel.sessions) { session in
-//                        ReoredringSessionCard(session: session, thresholds: thresholds, measurementStreamStorage: measurementStreamStorage, urlProvider: urlProvider)
-//                    }
-//                .onDelete(perform: viewModel.deleteItems)
-//                .onMove(perform: { indexSet, index in
-//                    viewModel.sessions.move(fromOffsets: indexSet, toOffset: index)
-//                })
-//            }
-//            .listStyle(.inset)
-//    }
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(viewModel.sessions) { session in
+                    ReoredringSessionCard(session: session, thresholds: thresholds, measurementStreamStorage: measurementStreamStorage, urlProvider: urlProvider)
+                        .overlay(viewModel.currentSession == session && changedView ? Color.white.opacity(0.8) : Color.clear)
+                        .onDrag({
+                            viewModel.currentSession = session
+                            changedView = false
+                            return NSItemProvider(object: String(describing: session.uuid) as NSString)
+                        })
+                        .onDrop(of: [.text], delegate: DropViewDelegate(session: session, currentSession: $viewModel.currentSession, sessions: $viewModel.sessions, changedView: $changedView))
+                }
+            }
+            .padding()
+        }
+        .background(Color.clear.edgesIgnoringSafeArea(.all)) // this is added to avoid session card staying 0.8 opaque when user drops card on the edges
+        .onDrop(of: [.text], delegate: DropOutsideOfGridDelegate(currentSession: $viewModel.currentSession))
+    }
 }
 
 struct ReorderingDashboard_Previews: PreviewProvider {
     static var previews: some View {
-        ReorderingDashboard(viewModel: ReorderingDashboardViewModel(sessions: [.mock, .mock]), thresholds: [.mock, .mock], measurementStreamStorage: PreviewMeasurementStreamStorage(), urlProvider: DummyURLProvider())
+        ReorderingDashboard(viewModel: ReorderingDashboardViewModel(sessions: [.mock, .mock, .mock]), thresholds: [.mock, .mock], measurementStreamStorage: PreviewMeasurementStreamStorage(), urlProvider: DummyURLProvider())
     }
 }
