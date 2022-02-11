a472c62058ae835fa928673516718e08f2f7a647
Janek Krzempek <jankrzempek9@gmail.com>
2022-02-11 11:24:56 +0000

    Improved StringCustomizer by splitting func into smaller parts.

--- AirCasting/Utils/StringsCustomizer.swift
+++ AirCasting/Utils/StringsCustomizer.swift
@@ -6,33 +6,56 @@ import SwiftUI
 final class StringCustomizer {
     static func customizeString(_ s: String, using words: [String], fontWeight: Font.Weight = .bold, color: Color = .aircastingGray, font: Font = .body, makeNewLineAfterCustomized: Bool = false) -> Text {
         
-        var descriptionText: Text!
+        var customizedText: Text!
         let separatedText = s.components(separatedBy: " ")
-        var newL = false
-        var keyWordsSeparated = words.flatMap { $0.components(separatedBy: " ") }
+        var newLine = false
+        var separatedKeyWords = words.flatMap { $0.components(separatedBy: " ") }
         
-        for word in separatedText {
-            if keyWordsSeparated.contains(word) {
-                if let index = keyWordsSeparated.firstIndex(of: word) {
-                    keyWordsSeparated.remove(at: index)
-                }
-                newL = true
-                descriptionText = (descriptionText == nil ? Text(word) : descriptionText + Text(word))
+        separatedText.forEach { word in
+            if separatedKeyWords.contains(word) {
+                removeUsedWord(separatedKeyWords: &separatedKeyWords, word: word)
+                newLine = true
+                customizedText = appended(word: word, to: customizedText)
                     .fontWeight(fontWeight)
                     .foregroundColor(color)
                     .font(font)
             } else {
-                if newL && makeNewLineAfterCustomized {
-                    descriptionText = descriptionText + Text("\n")
-                }
-                descriptionText = (descriptionText == nil ? Text(word) : descriptionText + Text(word))
+                customizedText = shouldAddEmptyLine(after: customizedText,
+                                                    (newLine && makeNewLineAfterCustomized))
+                customizedText = appended(word: word, to: customizedText)
                     .fontWeight(.regular)
                     .foregroundColor(.aircastingGray)
-                newL = false
+                newLine = false
             }
-            descriptionText = descriptionText + Text(" ")
+            customizedText = addSpace(after: customizedText)
         }
-        return descriptionText
+        return customizedText
+    }
+    
+    //MARK: - Private
+    private static func removeUsedWord(separatedKeyWords: inout [String], word: String) {
+        if let index = separatedKeyWords.firstIndex(of: word) {
+            separatedKeyWords.remove(at: index)
+        }
+    }
+    
+    private static func addSpace(after text: Text) -> Text {
+        text + Text(" ")
+    }
+    
+    private static func appended(word: String, to text: Text?) -> Text {
+        return (text == nil ? Text(word) : text! + Text(word))
+    }
+    
+    private static func shouldAddEmptyLine(after text: Text, _ should: Bool) -> Text {
+        guard should else {
+            return text
+        }
+        return addNewEmptyLine(after: text)
+    }
+    
+    private static func addNewEmptyLine(after text: Text) -> Text {
+        text + Text("\n")
     }
     
 }
