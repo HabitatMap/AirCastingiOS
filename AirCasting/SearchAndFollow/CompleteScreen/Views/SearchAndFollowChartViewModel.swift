90f846c28ce098c558f8b5004d259f4b18114ddc
anna1901 <anna.olak1901@gmail.com>
2022-04-15 16:21:24 +0200

    Show one hour averages on complete screen chart

--- AirCasting/SearchAndFollow/CompleteScreen/Views/SearchAndFollowChartViewModel.swift
+++ AirCasting/SearchAndFollow/CompleteScreen/Views/SearchAndFollowChartViewModel.swift
@@ -19,13 +19,32 @@ class SearchAndFollowChartViewModel: ObservableObject {
     let numberOfEntries = 9
     
     func generateEntries(with measurements: [ChartMeasurement], thresholds: ThresholdsValue) -> (Date?, Date?) {
-        // TODO: Rewrite this. We need to check how many measurements we have for a specific hour and then calculate the average. Separate ticket is added to trello
         var times: [Date] = []
-        entries = measurements.suffix(numberOfEntries).map {
-            times.append($0.time)
-            return ChartDot(value: $0.value, color: thresholds.colorFor(value: $0.value))
-        }
+        var buffer: [ChartMeasurement] = []
         
+        for measurement in measurements.reversed() {
+            if entries.count == 9 {
+                break
+            }
+            
+            if buffer.isEmpty || hourIsAlreadyPresent(in: buffer.map({ $0.time }), date: measurement.time) {
+                buffer.append(measurement)
+                continue
+            }
+            
+            let average = Int(buffer.map { $0.value }.reduce(0, +)) / buffer.count
+            
+            times.append(buffer.last!.time.roundedUpToHour)
+            entries.append(ChartDot(value: Double(average), color: thresholds.colorFor(value: measurement.value)))
+            buffer = [measurement]
+        }
+        entries.reverse()
         return (startTime: times.min(), endTime: times.max())
+    }
+    
+    private func hourIsAlreadyPresent(in times: [Date], date: Date) -> Bool {
+        let nowComponent = Calendar.current.component(.hour, from: date)
+        let hoursAlreadyPresent = times.map { Calendar.current.component(.hour, from: $0) }
+        return hoursAlreadyPresent.contains(nowComponent)
     }
 }
