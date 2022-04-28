6c4f59aad72d1356be29f0ce2686c9cf706814f5
jankrzempek <44276070+jankrzempek@users.noreply.github.com>
2022-04-28 08:42:59 +0200

    Adjusted code to be more generic and improved UI. (#680)
    * Adjusted code to be more generic and improved UI.

--- AirCasting/SearchAndFollow/SearchMap/SearchMapViewModel.swift
+++ AirCasting/SearchAndFollow/SearchMap/SearchMapViewModel.swift
@@ -22,8 +22,8 @@ enum PointerValue {
 class SearchMapViewModel: ObservableObject {
     let passedLocation: String
     let passedLocationAddress: CLLocationCoordinate2D
-    private let measurementType: MapDownloaderMeasurementType
-    private let sensorType: MapDownloaderSensorType
+    private let measurementType: MeasurementType
+    private let sensorType: SensorType
     @Injected private var mapSessionsDownloader: SessionsForLocationDownloader
     @Published var sessionsList = [MapSessionMarker]()
     @Published var searchAgainButton: Bool = false
@@ -34,7 +34,7 @@ class SearchMapViewModel: ObservableObject {
     @Published var shouldCardsScroll: Bool = false
     private var currentPosition: GeoSquare?
     
-    init(passedLocation: String, passedLocationAddress: CLLocationCoordinate2D, measurementType: MapDownloaderMeasurementType, sensorType: MapDownloaderSensorType) {
+    init(passedLocation: String, passedLocationAddress: CLLocationCoordinate2D, measurementType: MeasurementType, sensorType: SensorType) {
         self.passedLocation = passedLocation
         self.passedLocationAddress = passedLocationAddress
         self.measurementType = measurementType
@@ -78,8 +78,8 @@ class SearchMapViewModel: ObservableObject {
         mapSessionsDownloader.getSessions(geoSquare: geoSquare,
                                           timeFrom: timeFrom,
                                           timeTo: timeTo,
-                                          parameter: measurementType,
-                                          sensor: sensorType) { result in
+                                          measurementType: measurementType.downloaderType,
+                                          sensor: sensorType.downloaderType) { result in
             DispatchQueue.main.async { self.showLoadingIndicator = false }
             switch result {
             case .success(let sessions):
@@ -117,6 +117,25 @@ class SearchMapViewModel: ObservableObject {
             self.alert = InAppAlerts.downloadingSessionsFailedAlert {
                 self.shouldDismissView = true
             }
+        }
+    }
+}
+
+extension MeasurementType {
+    var downloaderType: MapDownloaderMeasurementType {
+        switch self {
+        case .particulateMatter: return .particulateMatter
+        case .ozone: return .ozone
+        }
+    }
+}
+
+extension SensorType {
+    var downloaderType: MapDownloaderSensorType {
+        switch self {
+        case .AB3and2: return .AB3and2
+        case .OpenAQ: return .OpenAQ
+        case .PurpleAir: return .PurpleAir
         }
     }
 }
