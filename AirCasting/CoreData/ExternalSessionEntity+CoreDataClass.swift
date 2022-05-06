cb9b83cd22c91edba4b43b154fcc1745893bb5c9
anna1901 <anna.olak1901@gmail.com>
2022-05-06 17:11:37 +0200

    Generate externalSessionEntity subclass

---
+++ AirCasting/CoreData/ExternalSessionEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ExternalSessionEntity+CoreDataClass.swift
+//  
+//
+//  Created by Lunar on 06/05/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ExternalSessionEntity)
+public class ExternalSessionEntity: NSManagedObject {
+
+}
