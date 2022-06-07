ca2aae809859f7cd4ba1c3dc044b2e446935a13a
Janek <jankrzempek9@gmail.com>
2022-06-07 10:49:30 +0200

    Proper measurements cut.

--- AirCasting/Services/RemoveOldMeasurementsService.swift
+++ AirCasting/Services/RemoveOldMeasurementsService.swift
@@ -12,7 +12,6 @@ final class RemoveOldMeasurementsService {
     }
     
     private static let twentyFourHoursMeasurementCount = 24 * 60
-    private static let openAQtwentyFourHoursMeasurementCount = 5 * 24
     
     /// In fixed sessions we need to remove measurements older than 24h, but we treat 24 not like a date, but as a sum of measurements.
     /// We know that we have 60 measurements per hour, so we take 1440 last measurements for each stream, and we remove older than the first of them.
@@ -26,22 +25,22 @@ final class RemoveOldMeasurementsService {
     }
     
     // PurpleAir — measurements from there are not consistent, so we need to do more 'manual work' 🔧
-    // OpenAQ — measurement every 12 min (5 times/1h): 120 measurements (per 24 hours)
+    // OpenAQ — measurements from there are not consistent, so we need to do more 'manual work' 🔧
+    // 'manual work' - taking the newest measurement and by having its time we are going back exactly 24 hours
     // AirBeam - measurement every 1 min (60 times/1h): 1440 measurements (per 24 hours)
     private func removeMeasurementsSurplus(context: NSManagedObjectContext, streams: [MeasurementStreamEntity], provider: String = "") throws {
         for stream in streams {
             guard let measurementsCount = stream.measurements?.count else { continue }
             
             switch provider {
-            case SensorType.PurpleAir.capitalizedName: defaultPurpleRemover(context: context, measurementsCount: measurementsCount, stream: stream); return
-            default: defaultRemover(for: provider, context: context, measurementsCount: measurementsCount, stream: stream); return
+            case SensorType.PurpleAir.capitalizedName, SensorType.OpenAQ.capitalizedName: timeBasedRemover(context: context, measurementsCount: measurementsCount, stream: stream); return
+            default: numberBasedRemover(for: provider, context: context, measurementsCount: measurementsCount, stream: stream); return
             }
         }
     }
     
-    private func defaultRemover(for sensor: String, context: NSManagedObjectContext, measurementsCount: Int, stream: MeasurementStreamEntity) {
-        var twentyFourMeasurementsCount = RemoveOldMeasurementsService.twentyFourHoursMeasurementCount
-        if sensor == SensorType.OpenAQ.capitalizedName { twentyFourMeasurementsCount = RemoveOldMeasurementsService.openAQtwentyFourHoursMeasurementCount }
+    private func numberBasedRemover(for sensor: String, context: NSManagedObjectContext, measurementsCount: Int, stream: MeasurementStreamEntity) {
+        let twentyFourMeasurementsCount = RemoveOldMeasurementsService.twentyFourHoursMeasurementCount
         if measurementsCount > twentyFourMeasurementsCount {
             let surplus = stream.allMeasurements?.prefix(measurementsCount - twentyFourMeasurementsCount) ?? []
             for measurement in surplus {
@@ -50,7 +49,7 @@ final class RemoveOldMeasurementsService {
         }
     }
     
-    private func defaultPurpleRemover(context: NSManagedObjectContext, measurementsCount: Int, stream: MeasurementStreamEntity) {
+    private func timeBasedRemover(context: NSManagedObjectContext, measurementsCount: Int, stream: MeasurementStreamEntity) {
         let lastMeasurement = stream.allMeasurements?.last
         let twentyFour = 86400000 // 24 hours in miliseconds: 60 * 60 * 24
         let beginingOfCorrectPeriod = lastMeasurement!.time.milliseconds - twentyFour
