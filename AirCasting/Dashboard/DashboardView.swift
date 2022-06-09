916fd77a3c7538dd2909a85e834f1ec100e0a255
szymon.gorowski96@gmail.com <szymon.gorowski96@gmail.com>
2022-06-09 17:48:54 +0200

    refactorig dashboard to allow user swipe between tabs

--- AirCasting/Dashboard/DashboardView.swift
+++ AirCasting/Dashboard/DashboardView.swift
@@ -14,16 +14,16 @@ import Resolver
 struct DashboardView: View {
     @StateObject var coreDataHook: CoreDataHook
     @FetchRequest<SensorThreshold>(sortDescriptors: [.init(key: "sensorName", ascending: true)]) var thresholds
-    @EnvironmentObject var selectedSection: SelectSection
+    // TODO: - We can remove it in a future
+    @EnvironmentObject var selectedSection_: SelectSection
     @EnvironmentObject var reorderButton: ReorderButton
     @EnvironmentObject var searchAndFollowButton: SearchAndFollowButton
     @State var isRefreshing: Bool = false
-    @State private var isSwipingLeft: Bool = false
+    @State var selectedSection: SelectedSection = .following
     @Injected private var sessionSynchronizer: SessionSynchronizer
+    @Injected private var persistenceController: PersistenceController
     @InjectedObject private var featureFlagsViewModel: FeatureFlagsViewModel
-
-    private let dashboardCoordinateSpaceName = "dashboardCoordinateSpace"
-
+    
     private var sessions: [Sessionable] {
         coreDataHook.sessions
     }
@@ -48,34 +48,26 @@ struct DashboardView: View {
                                     thresholds: Array(self.thresholds))
             } else {
                 sessionTypePicker
-                Group {
-                    if selectedSection.selectedSection == .following {
-                        followingTab
-                            .transition(AnyTransition.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
-                    } else if selectedSection.selectedSection == .mobileActive {
-                        mobileActiveTab
-                            .transition(AnyTransition.asymmetric(insertion: .move(edge: isSwipingLeft ? .trailing : .leading), removal: .move(edge: isSwipingLeft ? .leading : .trailing)))
-                    } else if selectedSection.selectedSection == .mobileDormant {
-                        mobileDormantTab
-                            .transition(AnyTransition.asymmetric(insertion: .move(edge: isSwipingLeft ? .trailing : .leading), removal: .move(edge: isSwipingLeft ? .leading : .trailing)))
-                    } else if selectedSection.selectedSection == .fixed {
-                        fixedTab
-                            .transition(AnyTransition.asymmetric(insertion: .move(edge: isSwipingLeft ? .trailing : .leading), removal: .move(edge: isSwipingLeft ? .leading : .trailing)))
+                TabView(selection: $selectedSection) {
+                    ForEach(SelectedSection.allCases, id: \.self) {
+                        SessionsListView(selectedSection: $0, isRefreshing: $isRefreshing, context: persistenceController.viewContext)
                     }
                 }
+                .tabViewStyle(.page(indexDisplayMode: .never))
             }
         }
-        .gesture(drag)
         .fullScreenCover(isPresented: $searchAndFollowButton.searchIsOn) {
             CreatingSessionFlowRootView {
                 SearchView(isSearchAndFollowLinkActive: $searchAndFollowButton.searchIsOn)
             }
         }
         .navigationBarTitle(Strings.DashboardView.dashboardText)
-        .onChange(of: selectedSection.selectedSection) { selectedSection in
-            self.selectedSection.selectedSection = selectedSection
-            try! coreDataHook.setup(selectedSection: self.selectedSection.selectedSection)
+        .onChange(of: selectedSection_.selectedSection) { selectedSection in
+            self.selectedSection_.selectedSection = selectedSection
         }
+        .onChange(of: selectedSection, perform: { newValue in
+            self.selectedSection_.selectedSection = newValue
+        })
         .onChange(of: isRefreshing, perform: { newValue in
             guard newValue == true else { return }
             guard !sessionSynchronizer.syncInProgress.value else {
@@ -85,14 +77,14 @@ struct DashboardView: View {
             sessionSynchronizer.triggerSynchronization() { isRefreshing = false }
         })
         .onAppear() {
-            try! coreDataHook.setup(selectedSection: self.selectedSection.selectedSection)
+            try! coreDataHook.setup(selectedSection: self.selectedSection_.selectedSection)
             reorderButton.isHidden = false
             searchAndFollowButton.isHidden = false
         }
     }
 
     private var sessionTypePicker: some View {
-        AirSectionPickerView(selection: self.$selectedSection.selectedSection)
+        AirSectionPickerView(selection: self.$selectedSection)
             .padding(.leading)
             .background(
                 ZStack(alignment: .bottom) {
@@ -128,81 +120,6 @@ struct DashboardView: View {
         .zIndex(2)
     }
     
-    private var mobileActiveTab: some View {
-        Group {
-            if sessions.isEmpty { emptySessionsView } else { sessionListView }
-        }
-    }
-    
-    private var followingTab: some View {
-        Group {
-            if sessions.isEmpty { emptySessionsView } else { sessionListView }
-        }
-    }
-    
-    private var mobileDormantTab: some View {
-        Group {
-            if sessions.isEmpty { emptySessionsView } else { sessionListView }
-        }
-    }
-    
-    private var fixedTab: some View {
-        Group {
-            if sessions.isEmpty { emptySessionsView } else { sessionListView }
-        }
-    }
-    
-    private var emptySessionsView: some View {
-        GeometryReader { geometry in
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    RefreshControl(coordinateSpace: .named(dashboardCoordinateSpaceName), isRefreshing: $isRefreshing)
-                    if selectedSection.selectedSection == .mobileActive || selectedSection.selectedSection == .mobileDormant {
-                        EmptyMobileDashboardViewMobile()
-                            .frame(height: geometry.size.height)
-                    } else {
-                        EmptyFixedDashboardView()
-                            .frame(height: geometry.size.height)
-                    }
-                }
-            }
-            .coordinateSpace(name: dashboardCoordinateSpaceName)
-            .background(Color.aliceBlue)
-        }
-    }
-    
-    private var sessionListView: some View {
-        ZStack(alignment: .bottomTrailing) {
-            Image("dashboard-background-thing")
-            let thresholds = Array(self.thresholds)
-            ScrollView {
-                RefreshControl(coordinateSpace: .named(dashboardCoordinateSpaceName), isRefreshing: $isRefreshing)
-                LazyVStack(spacing: 8) {
-                    ForEach(sessions.filter { $0.uuid != "" && !$0.gotDeleted }, id: \.uuid) { session in
-                        if session.isExternal && featureFlagsViewModel.enabledFeatures.contains(.searchAndFollow) {
-                            if let entity = session as? ExternalSessionEntity {
-                                ExternalSessionCard(session: entity, thresholds: thresholds)
-                            }
-                        } else {
-                            if let entity = session as? SessionEntity {
-                                let followingSetter = MeasurementStreamStorageFollowingSettable(session: entity)
-                                let viewModel = SessionCardViewModel(followingSetter: followingSetter)
-                                SessionCardView(session: entity,
-                                                sessionCartViewModel: viewModel,
-                                                thresholds: thresholds
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-            .coordinateSpace(name: dashboardCoordinateSpaceName)
-        }
-        .padding(.horizontal)
-        .frame(maxWidth: .infinity)
-        .background(Color.aircastingGray.opacity(0.05))
-    }
-    
     private func onCurrentSyncEnd(_ completion: @escaping () -> Void) {
         guard sessionSynchronizer.syncInProgress.value else { completion(); return }
         var cancellable: AnyCancellable?
@@ -212,56 +129,7 @@ struct DashboardView: View {
             cancellable?.cancel()
         }
     }
-    
-    private func showPreviousTab() {
-        switch selectedSection.selectedSection {
-        case .following:
-            break
-        case .mobileActive:
-            selectedSection.selectedSection = .following
-        case .mobileDormant:
-            selectedSection.selectedSection = .mobileActive
-        case .fixed:
-            selectedSection.selectedSection = .mobileDormant
-        }
-    }
-    
-    private func showNextTab() {
-        switch selectedSection.selectedSection {
-        case .following:
-            selectedSection.selectedSection = .mobileActive
-        case .mobileActive:
-            selectedSection.selectedSection = .mobileDormant
-        case .mobileDormant:
-            selectedSection.selectedSection = .fixed
-        case .fixed:
-            break
-        }
-    }
-    
-    private func handleSwipe(translation: CGFloat) {
-        let minDragTranslationForSwipe: CGFloat = 50
-        if translation > minDragTranslationForSwipe {
-            showPreviousTab()
-            isSwipingLeft = false
-            Log.info("\(isSwipingLeft)")
-        } else  if translation < -minDragTranslationForSwipe {
-            showNextTab()
-            isSwipingLeft = true
-            Log.info("\(isSwipingLeft)")
-        }
-    }
-    
-    private var drag: some Gesture {
-        DragGesture()
-            .onEnded { value in
-                withAnimation(.easeInOut(duration: 0.2)) {
-                    self.handleSwipe(translation: value.translation.width)
-                }
-            }
-    }
 }
-
 
 @available(iOS, deprecated: 15, obsoleted: 15, message: "Please review if this is still needed")
 struct PreventCollapseView: View {
---
+++ AirCasting/Dashboard/SessionListView.swift
@@ -0,0 +1,89 @@
+//
+//  SessionsListView.swift
+//  
+//
+//  Created by lunar  on 09/06/2022.
+//
+
+import SwiftUI
+import Resolver
+import AirCastingStyling
+import CoreData
+
+struct SessionsListView: View {
+    @FetchRequest<SensorThreshold>(sortDescriptors: [.init(key: "sensorName", ascending: true)]) private var thresholds
+    @StateObject private var coreDataHook: CoreDataHook
+    @Binding var isRefreshing: Bool
+    @InjectedObject private var featureFlagsViewModel: FeatureFlagsViewModel
+    
+    private let listCoordinateSpaceName = "listCoordinateSpace"
+    let selectedSection: SelectedSection
+
+    init(selectedSection: SelectedSection, isRefreshing: Binding<Bool>, context: NSManagedObjectContext) {
+        self.selectedSection = selectedSection
+        _isRefreshing = .init(projectedValue: isRefreshing)
+        _coreDataHook = .init(wrappedValue: .init(context: context))
+    }
+    
+    var body: some View {
+        VStack {
+            if coreDataHook.sessions.isEmpty {
+                emptySessionsView
+            } else {
+                sessionListView
+            }
+        }
+        .onAppear { try! coreDataHook.setup(selectedSection: selectedSection) }
+    }
+    
+    private var sessionListView: some View {
+        ZStack(alignment: .bottomTrailing) {
+            Image("dashboard-background-thing")
+            let thresholds = Array(self.thresholds)
+            ScrollView {
+                RefreshControl(coordinateSpace: .named(listCoordinateSpaceName), isRefreshing: $isRefreshing)
+                LazyVStack(spacing: 8) {
+                    ForEach(coreDataHook.sessions.filter { $0.uuid != "" && !$0.gotDeleted }, id: \.uuid) { session in
+                        if session.isExternal && featureFlagsViewModel.enabledFeatures.contains(.searchAndFollow) {
+                            if let entity = session as? ExternalSessionEntity {
+                                ExternalSessionCard(session: entity, thresholds: thresholds)
+                            }
+                        } else {
+                            if let entity = session as? SessionEntity {
+                                let followingSetter = MeasurementStreamStorageFollowingSettable(session: entity)
+                                let viewModel = SessionCardViewModel(followingSetter: followingSetter)
+                                SessionCardView(session: entity,
+                                                sessionCartViewModel: viewModel,
+                                                thresholds: thresholds
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            .coordinateSpace(name: listCoordinateSpaceName)
+        }
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity)
+        .background(Color.aircastingGray.opacity(0.05))
+    }
+    
+    private var emptySessionsView: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    RefreshControl(coordinateSpace: .named(listCoordinateSpaceName), isRefreshing: $isRefreshing)
+                    if selectedSection == .mobileActive || selectedSection == .mobileDormant {
+                        EmptyMobileDashboardViewMobile()
+                            .frame(height: geometry.size.height)
+                    } else {
+                        EmptyFixedDashboardView()
+                            .frame(height: geometry.size.height)
+                    }
+                }
+            }
+            .coordinateSpace(name: listCoordinateSpaceName)
+            .background(Color.aliceBlue)
+        }
+    }
+}
