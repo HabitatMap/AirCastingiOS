bf64d2397c3a0994e10ff10b5cbd436c51d05146
Janek <jankrzempek9@gmail.com>
2022-06-20 12:38:55 +0200

    removed unused code.

--- AirCasting/SearchAndFollow/AirBeamMeasurements/AirBeamStreamSuffixes.swift
+++ AirCasting/SearchAndFollow/AirBeamMeasurements/AirBeamStreamSuffixes.swift
@@ -10,16 +10,6 @@ enum AirBeamStreamSuffixes: CaseIterable {
     case pm10
     case RH
     
-    var rawName: String {
-        switch self {
-        case .F: return "f"
-        case .pm1: return "pm1"
-        case .pm25: return "pm2.5"
-        case .pm10: return "pm10"
-        case .RH: return "rh"
-        }
-    }
-    
     var capitalizedName: String {
         switch self {
         case .F: return "F"
