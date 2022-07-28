43737895773d38ce35ca6e7276fb6fda22ab68d1
Anna Olak <anna.olak1901@gmail.com>
2022-07-28 16:22:16 +0200

    Change authorization no connection alert message (#828)

--- AirCasting/AuthViews/CreateAccountView.swift
+++ AirCasting/AuthViews/CreateAccountView.swift
@@ -147,7 +147,7 @@ private extension CreateAccountView {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
             
             guard networkChecker.connectionAvailable else {
-                alert = InAppAlerts.noNetworkAlert()
+                alert = InAppAlerts.noNetworkAuthorizationAlert()
                 return
             }
             
