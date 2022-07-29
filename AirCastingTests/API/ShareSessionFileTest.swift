f91b9ece48e8afe02fd0928a55d1003b23fa38ec
anna1901 <anna.olak1901@gmail.com>
2022-07-29 16:24:47 +0200

    Split test into two

--- AirCastingTests/API/ShareSessionFileTest.swift
+++ AirCastingTests/API/ShareSessionFileTest.swift
@@ -2,13 +2,12 @@
 //
 
 import XCTest
-import Resolver
 @testable import AirCasting
 
 class ShareSessionFileTest: APIServiceTestCase {
     lazy var sut = ShareSessionApi()
 
-    func test_createsCorrectRequest() throws {
+    func test_whenSendsRequest_hasCorrectURL() throws {
         var receivedRequest: URLRequest?
         client.requestTaskStub = { request, completion in
             receivedRequest = request
@@ -21,7 +20,20 @@ class ShareSessionFileTest: APIServiceTestCase {
 
         let request = try XCTUnwrap(receivedRequest)
         XCTAssertEqual(request.url, URL(string: "http://aircasting.org/api/sessions/export_by_uuid.json?email=\(email)&uuid=\(uuid)")!)
+    }
+    
+    func test_whenSendsRequest_usesGETMethod() throws {
+        var receivedRequest: URLRequest?
+        client.requestTaskStub = { request, completion in
+            receivedRequest = request
+            completion(.failure(URLError(.timedOut)), request)
+        }
+        
+        let email = "test@test.com"
+        let uuid = UUID().uuidString
+        sut.sendSession(email: email, uuid: uuid, completion: { _ in })
+
+        let request = try XCTUnwrap(receivedRequest)
         XCTAssertEqual(request.httpMethod, "GET")
     }
-
 }
