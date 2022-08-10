903b00e5960906f52d72741cda4dc4b54ccf8534
Janek <jankrzempek9@gmail.com>
2022-08-10 09:45:20 +0200

    Feature flags default value changes to always true.

--- AirCasting/FeatureFlags/FeatureFlagProviders/DefaultFeatureFlagProvider.swift
+++ AirCasting/FeatureFlags/FeatureFlagProviders/DefaultFeatureFlagProvider.swift
@@ -3,43 +3,8 @@ class DefaultFeatureFlagProvider: FeatureFlagProvider {
     
     func isFeatureOn(_ feature: FeatureFlag) -> Bool? {
         switch feature {
-        case .sdCardSync:
-            #if DEBUG
+        default:
             return true
-            #else
-            return false
-            #endif
-        case .standaloneMode:
-            #if DEBUG
-            return true
-            #else
-            return false
-            #endif
-        case .notes:
-            #if DEBUG
-            return true
-            #else
-            return false
-            #endif
-        case .locationlessSessions:
-            #if DEBUG
-            return true
-            #else
-            return false
-            #endif
-        case .searchAndFollow:
-            #if DEBUG
-            return true
-            #else
-            return false
-            #endif
-        case .deleteAccount:
-            #if DEBUG
-            return true
-            #else
-            return false
-            #endif
         }
-        
     }
 }
