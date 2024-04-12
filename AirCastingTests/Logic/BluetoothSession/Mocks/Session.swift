07b3abf3ac36ac1ee3ee60a1dfbd27fdc52bc2aa
Janek <jankrzempek9@gmail.com>
2024-04-12 17:57:17 +0200

    Adjusting tests to the latest changes in code.

--- AirCastingTests/Logic/BluetoothSession/Mocks/Session.swift
+++ AirCastingTests/Logic/BluetoothSession/Mocks/Session.swift
@@ -5,7 +5,7 @@ import Foundation
 @testable import AirCasting
 
 extension Session {
-    static let mobileAirBeamMock = Session(uuid: "123", type: .mobile, name: "Mobile Session", deviceType: .AIRBEAM3, location: .undefined, startTime: DateBuilder.getFakeUTCDate())
+    static let mobileAirBeamMock = Session(uuid: "123", type: .mobile, name: "Mobile Session", deviceType: .AIRBEAM, location: .undefined, startTime: DateBuilder.getFakeUTCDate())
     
-    static let mobileAirBeamLocationlessMock = Session(uuid: "123", type: .mobile, name: "Mobile Session", deviceType: .AIRBEAM3, location: .undefined, startTime: DateBuilder.getFakeUTCDate(), contribute: false, locationless: true)
+    static let mobileAirBeamLocationlessMock = Session(uuid: "123", type: .mobile, name: "Mobile Session", deviceType: .AIRBEAM, location: .undefined, startTime: DateBuilder.getFakeUTCDate(), contribute: false, locationless: true)
 }
