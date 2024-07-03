8ed495a9eeb52fd47721bbdcef6af20181f2e89c
Marta Panuszewska <marta.panuszewska@lunarlogic.io>
2024-07-03 11:49:57 +0200

    FIxes

--- AirCasting/ABConnector/MeasurementsRecordingServices.swift
+++ AirCasting/ABConnector/MeasurementsRecordingServices.swift
@@ -99,9 +99,9 @@ class AirbeamMeasurementsRecordingServices: MeasurementsRecordingServices {
 
         guard let value else { return }
         
-        let components = value.components(separatedBy: " ")
-        guard components.count > 3 else { return }
-        let percentage = components[2] // e.g. 53%
+        let components = value.components(separatedBy: " ").filter { !$0.isEmpty }
+        guard components.count >= 3 else { return }
+        let percentage = components[1] // e.g. 53%
         guard let percentageNumber = Int(percentage.dropLast(1)) else { return } // e.g. 53
         let range = currentBatteryLvl-2...currentBatteryLvl+1
         
