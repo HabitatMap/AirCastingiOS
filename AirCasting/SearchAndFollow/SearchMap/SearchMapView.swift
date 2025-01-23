eb1215ccf62efae0176163e74ca1db8996a17be7
Marta Panuszewska <marta.panuszewska@lunarlogic.io>
2025-01-23 15:42:37 +0100

    fix: Disable rerouting to dashboard tab after finishing searching for sessions

--- AirCasting/SearchAndFollow/SearchMap/SearchMapView.swift
+++ AirCasting/SearchAndFollow/SearchMap/SearchMapView.swift
@@ -237,8 +237,11 @@ private extension SearchMapView {
     var finishButton: some View {
         Button {
             isSearchAndFollowLinkActive = false
-            tabSelection.update(to: .dashboard)
-            selectedSection.section = .following
+            // The following were commented out because they were causing app to freeze on iOS18
+            // Issue connected to and described in https://github.com/HabitatMap/AirCastingiOS/pull/1049
+            // FIX ASAP
+            // tabSelection.update(to: .dashboard)
+            // selectedSection.section = .following
         } label: {
             Text(Strings.SearchMapView.finishText)
                 .font(Fonts.muliRegularHeading3.bold())
