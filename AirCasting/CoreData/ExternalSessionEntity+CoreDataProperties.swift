a151e368b8dab97261ce099b7aaf5c53a2af86ca
Marta Panuszewska <mpanuszewska@gmail.com>
2025-02-11 14:41:07 +0100

    fix: Show PM1 stream in external sessions as well (#1058)

--- AirCasting/CoreData/ExternalSessionEntity+CoreDataProperties.swift
+++ AirCasting/CoreData/ExternalSessionEntity+CoreDataProperties.swift
@@ -71,13 +71,7 @@ extension ExternalSessionEntity : Identifiable {
 extension ExternalSessionEntity {
     
     var pm1Stream: MeasurementStreamEntity? {
-        let match = measurementStreams?.first(where: { (stream) -> Bool in
-            (stream as? MeasurementStreamEntity)?.sensorName == "AirBeam3-PM1" ||
-            (stream as? MeasurementStreamEntity)?.sensorName == "AirBeam2-PM1" ||
-            (stream as? MeasurementStreamEntity)?.sensorName == "AirBeam1-PM1"
-        })
-        let pm1Stream = match as? MeasurementStreamEntity
-        return pm1Stream
+        streamWithSensorNameContaining("PM1")
     }
     
     var pm2Stream: MeasurementStreamEntity? {
